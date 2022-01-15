6f4b290f9437cd0aaa2ccc34afe413c1c4682251
Jerem <jersou@gmail.com>
2022-01-15 11:27:57 +0100

    skip version : dl artifact

--- skip.sh
+++ skip.sh
@@ -10,6 +10,7 @@
 #   - if the nested jobs of current uses the dependencies key with current, the dependencies files need to be in an artifact
 #   - CI variable changes are not detected
 #   - need API_READ_TOKEN (personal access tokens that have read_api scope)
+#   - set GIT_DEPTH variable to 1000 or more
 if [[ "$SKIP_IF_TREE_OK_IN_PAST" = "" ]]; then
   echo -e "\e[1;41;39m    ⚠️ The SKIP_IF_TREE_OK_IN_PAST variable is empty, set the list of paths to check    \e[0m"
   exit 1
@@ -25,17 +26,21 @@ fi
 
 current_tree_sha=$(git ls-tree HEAD -- $SKIP_IF_TREE_OK_IN_PAST | tr / \| | git mktree)
 
-curl --silent --fail "$CI_SERVER_URL/api/v4/projects/${CI_PROJECT_ID}/jobs?scope=success&per_page=1000&page=&private_token=${API_READ_TOKEN}" |
-  fx ".filter(job => job.name === '$CI_JOB_NAME').map(job => job.commit.id + ' ' + job.id + ' ' + job.web_url).join('\n')" |
-  while read commit job web_url; do
-    # TODO : DL artifact && unzip
-    # TODO : do not skip if artifact deprecat
-
+curl --silent --fail "$CI_API_V4_URL/projects/${CI_PROJECT_ID}/jobs?scope=success&per_page=1000&page=&private_token=${API_READ_TOKEN}" |
+  fx ".filter(job => job.name === '$CI_JOB_NAME').map(j => [j.commit.id, j.web_url, j.artifacts_expire_at].join(' ')).join('\n')" |
+  while read commit  web_url  artifacts_expire_at ; do
     tree_sha=$(git ls-tree $commit -- $SKIP_IF_TREE_OK_IN_PAST | tr / \| | git mktree)
     if [[ "$tree_sha" = "$current_tree_sha" ]]; then
+      if [[ "$artifacts_expire_at" != "" ]]; then
+        echo  "artifacts_expire_at: $artifacts_expire_at"
+        curl -o artifact.zip "$web_url/artifacts/download" || break
+        unzip artifact.zip
+        rm artifact.zip
+        ls -al
+      fi
       echo -e "\e[1;43;30m    ✅ $current_tree_sha tree found in job $web_url   \e[0m"
       echo true >/tmp/ci-skip
-      break
+     # break
     fi
   done
 
