945e50fb8984581dcd60f46dc562bad4ffc64a52
Jerem <jersou@gmail.com>
2022-01-15 01:53:05 +0100

    API query version: refactor

--- check-ok-in-past.sh
+++ check-ok-in-past.sh
@@ -2,22 +2,15 @@
 
 current_tree_sha=$(git ls-tree HEAD -- "$@" | tr / \| | git mktree)
 echo current_tree_sha=$current_tree_sha
-found=""
 commits=$(curl --fail "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/jobs?scope=success&per_page=1000&page=&private_token=$TOKEN" |
-  fx ".filter(job => job.name==='$CI_JOB_NAME').map(job=>  job.commit.id).join('\n')")
+  fx ".filter(job => job.name === '$CI_JOB_NAME').map(job => job.commit.id).join('\n')")
 for commit in $commits; do
   tree_sha=$(git ls-tree $commit -- "$@" | tr / \| | git mktree)
-  # echo "commit $commit : tree_sha=$tree_sha"
   if [[ "$tree_sha" = "$current_tree_sha" ]]; then
-    found="$commit"
-    break
+    echo "✅ tree found in commit $found"
+    exit 0
   fi
 done
 
-if [[ "$found" = "" ]]; then
-  echo "❌ tree not found in last 1000 success jobs of the project"
-  exit 1
-else
-  echo "✅ tree found in commit $found"
-fi
-
+echo "❌ tree not found in last 1000 success jobs of the project"
+exit 1
