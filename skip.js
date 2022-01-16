5b11cd999754a25025aef22a3785271ff80b2510
Jerem <jersou@gmail.com>
2022-01-17 00:12:37 +0100

    skip-node-version

--- skip.js
+++ skip.js
@@ -95,6 +95,7 @@ function downloadFile(path, url) {
     client
       .get(url, (res) => {
         if (res.statusCode !== 200) {
+          console.error(res);
           reject(`Status Code: ${res.statusCode} !`);
         }
         res.pipe(file);
@@ -135,7 +136,7 @@ async function extractArtifacts(job) {
 
 async function main() {
   const current_tree = getTree("HEAD");
-  console.log(`current_tree:\n${current_tree}\n------------`);
+  //console.log(`current_tree:\n${current_tree}\n------------`);
   const projectJobs = await fetchJson(
     `${process.env.CI_API_V4_URL}/projects/${process.env.CI_PROJECT_ID}/jobs?scope=success&per_page=1000&page=&private_token=${process.env.API_READ_TOKEN}`
   );
